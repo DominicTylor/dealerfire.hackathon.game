2b8dc508ccb46d60b7507275c0dad7591f33cf5f
Valerii Ten <vTen@dealersocket.com>
2018-09-28 09:47:52 +0200

    fix energy drink type; add dispose backpack method

--- src/actors/energy-drink.js
+++ src/actors/energy-drink.js
@@ -4,7 +4,7 @@ export default class EnergyDrink extends Stuff {
 	constructor(scene, x, y) {
 		super(scene, x, y);
 
-		this.type = 'energy-drink';
+		this.type = 'energyDrink';
 		this.value = 20;
 		this.sprite = scene.physics.add.sprite(x, y, 'energy-drink');
 		this.sprite.body.immovable = true;
--- src/actors/manager.js
+++ src/actors/manager.js
@@ -1,3 +1,5 @@
+import Stuff from './stuff';
+
 export default class Manager {
 	constructor(scene, x, y) {
 		this._speed = 100;
@@ -82,7 +84,7 @@ export default class Manager {
 
 	interact(me, slave) {
 		if (this.backpack !== null && slave.consume(this.backpack)) {
-			this.backpack = null;
+			this.destroyBackpackItem();
 		}
 
 		if (slave instanceof Manager) {
@@ -94,6 +96,13 @@ export default class Manager {
 		stuff.destroy();
 
 		return true;
+	}
+
+	destroyBackpackItem() {
+		if (this.backpack && this.backpack instanceof Stuff) {
+			this.backpack.destroy();
+			this.backpack = null;
+		}
 	}
 
 	update() {
@@ -125,7 +134,7 @@ export default class Manager {
 		}
 
 		if (this.keys.space.isDown) {
-			this.backpack = null;
+			this.destroyBackpackItem();
 		}
 
 		if (this.backpack !== null) {
